9c53a074c76fa4464ed80b0d90dea652474a0792
programdog <531353008@qq.com>
2019-01-08 11:28:49 +0800

    1.add fold : rzy_tools. 2.build project : rzy_com

---
+++ tools_test/rzy_com/rzy_com.c
@@ -0,0 +1,35 @@
+/**
+ * @Author   rzy
+ * @DateTime 2019-01-07T18:40:51+0800
+ * @Function :
+ * @return   [com tool]
+ */
+#include "rzy_com.h"
+
+int main(int argc, char **argv)
+{
+	int opt;
+	char user_port_name[16] = {0};
+	uint32_t user_baudrate = DEAUFLT_BAUDRATE;
+
+	while ((opt = getopt(argc, argv, "b:p:")) != EOF)
+	{
+		switch (opt)
+		{
+			case 'b':
+				user_baudrate = (uint32_t)atoi(optarg);
+			break;
+
+			case 'p':
+				snprintf(user_port_name, sizeof(user_port_name), "%s", optarg);
+			break;
+
+			default:
+				printf("only support -p -s\n -p : select which port\n -s : select which baudrate\r\n");
+				exit(0);
+		}
+	}
+	printf("user_port_name=%s, user_baudrate=%d\r\n", user_port_name, user_baudrate);
+	return 0;
+}
+
---
+++ tools_test/rzy_com/rzy_com.h
@@ -0,0 +1,16 @@
+#ifndef RZY_COM_H_
+#define RZY_COM_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+int main(int argc, char **argv);
+
+#define DEAUFLT_BAUDRATE 57600
+
+#endif
